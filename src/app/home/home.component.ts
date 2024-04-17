5bb6f5b4d00c636821f8ea6b97114cafbf5e8242
MarwaRz <129842893+MarwaRz@users.noreply.github.com>
2024-04-18 00:58:08 +0200

    Update home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+p1 = ' Bienvenue sur le Site de Réunions ! Notre plateforme a pour objectif de simplifier la planification et la gestion de vos réunions, 
+  que ce soit pour des petites réunions d'équipe ou des grands événements professionnels. Avec notre outil convivial, vous pouvez créer des agendas, inviter des participants, gérer les inscriptions et bien plus encore, le tout en un seul endroit. Gagnez du temps et de l'efficacité dans l'organisation de vos réunions en utilisant notre service. Découvrez dès maintenant comment nous pouvons vous aider à rendre vos réunions plus productives et sans tracas.' ;
 }
